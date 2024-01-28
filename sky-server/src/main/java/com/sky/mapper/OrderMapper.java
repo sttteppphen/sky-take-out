06d5bfa8ec982158fe2950535973ba4c05d518ab
sttteppphen <fzhcurry30@gmail.com>
2024-01-28 15:22:52 -0500

    地址簿功能代码

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,33 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface OrderMapper {
+    void insert(Orders orders);
+
+    /**
+     * 分页条件查询并按下单时间排序
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 根据id查询订单
+     * @param id
+     * @return
+     */
+    @Select("select * from orders where id =#{id}")
+    Orders getById(Long id);
+
+    /**
+     * 用户取消更新数据库
+     * @param orders
+     */
+    void update(Orders orders);
+}
---
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -0,0 +1,51 @@
+package com.sky.service;
+
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
+import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
+
+public interface OrderService {
+    /**
+     * 用户下单
+     * @param ordersSubmitDTO
+     * @return
+     */
+    OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO);
+
+    /**
+     * 历史订单查询
+     * @param page
+     * @param pageSize
+     * @param status
+     * @return
+     */
+    PageResult pageQuery4User(int page, int pageSize, Integer status);
+
+    /**
+     * 查询订单详情
+     * @param id
+     * @return
+     */
+    OrderVO detail(Long id);
+
+    /**
+     * 用户取消订单
+     * @param id
+     */
+    void userCancelById(Long id) throws Exception;
+
+    /**
+     * 再来一单
+     * @param id
+     */
+    void repetition(Long id);
+
+    /**
+     * 订单搜索
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
+}
